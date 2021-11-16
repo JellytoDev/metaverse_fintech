f47809ccaaa377465a9bddc2160b3c46e9993805
JellytoDev <wjdwhdgus2222@naver.com>
2021-11-16 17:47:21 +0900

    캐릭터 이동, 로그인 화면 구현

---
+++ Assets/Boy/Scripts/CharacterMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed = 10.0f;
+    float hAxis;
+    float vAxis;
+
+    Vector3 moveVec;
+
+    Animator anim;
+
+    int aniNum = 1;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+
+        if (!(hAxis == 0 && vAxis == 0))
+        {
+            transform.position += moveVec * speed * Time.deltaTime;
+            transform.LookAt(transform.position + moveVec);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(moveVec), Time.deltaTime * rotateSpeed);
+        }
+        
+
+    출처: https://mayquartet.tistory.com/44 [MayQing Study]
+
+        if (hAxis != 0 ||vAxis != 0)
+        {
+            aniNum = 2;
+            anim.SetInteger("aniKind", aniNum);
+        }
+        else
+        {
+            aniNum = 1;
+            anim.SetInteger("aniKind", aniNum);
+        }
+
+
+    }
+}
---
+++ Assets/Boy/Scripts/MoveCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("newboy").transform;
+        print(target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+        
+    }
+}
